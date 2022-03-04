7472bfbbbefd5162c244635188711fa6c1b14e90
carbon-hvze <6391474+carbon-hvze@users.noreply.github.com>
2022-03-04 14:38:06 +0300

    process multiline msgs, parse json

--- lib/telecms_web/td/backend.ex
+++ lib/telecms_web/td/backend.ex
@@ -11,22 +11,35 @@ defmodule TelecmsWeb.Td.Backend do
     {_res, root_path} = File.cwd()
     tdlib_path = Application.fetch_env!(:telecms, :binary_path)
 
-    state = %{port: Port.open({:spawn_executable, root_path <> tdlib_path}, [:binary, :line])}
+    state = %{
+      port: Port.open({:spawn_executable, root_path <> tdlib_path}, [:binary, :line]),
+      io: []
+    }
 
     {:ok, state}
   end
 
   @impl true
-  def handle_info({_from, {:data, data}}, state) do
-    ## TODO add multiline msg
+  def handle_info({_port, {:data, data}}, state) do
     case data do
-      {:eol, msg} -> pipe_data msg
+      {:eol, msg} ->
+        %{io: buffer} = state
+        pipe_data(buffer ++ [msg])
+        {:noreply, Map.replace(state, :io, [])}
 
+      {:noeol, chunk} ->
+        %{io: io} = state
+        {:noreply, Map.replace(state, :io, io ++ [chunk])}
+
+      shape ->
+        IO.warn("Unknown tdlib msg shape")
+        IO.inspect(shape)
+        {:noreply, state}
     end
-    {:noreply, state}
   end
 
   def pipe_data(msg) do
-    GenServer.cast :client, msg
+    Jason.decode!(msg) |>
+    then(fn parsed -> GenServer.cast :client, parsed end)
   end
 end
