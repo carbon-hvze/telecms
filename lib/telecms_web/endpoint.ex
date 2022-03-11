28ce8d8ccbfcf3955f902847478e604b11543bbb
carbon-hvze <6391474+carbon-hvze@users.noreply.github.com>
2022-03-11 14:00:16 +0300

    drop texts, prepare UI

--- lib/telecms_web/endpoint.ex
+++ lib/telecms_web/endpoint.ex
@@ -1,9 +1,7 @@
 defmodule TelecmsWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :telecms
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
+  # TODO Set :encryption_salt to encrypt session
   @session_options [
     store: :cookie,
     key: "_telecms_key",
@@ -12,18 +10,13 @@ defmodule TelecmsWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
+  # TODO package with phx.digest, gzip statics
   plug Plug.Static,
     at: "/",
     from: :telecms,
     gzip: false,
     only: ~w(assets fonts images favicon.ico robots.txt)
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
